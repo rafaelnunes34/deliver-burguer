7b24e3deab8887b529b1e055dee137380c59b810
rafaelnunes34 <rafael.nunes34@gmail.com>
2024-04-22 23:27:09 -0300

    Acertando URL backend

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://www.rnsolucoes.icu/',
+  apiUrl: 'https://www.rnsolucoes.icu',
   clienteId: 'deliverburguer',
   clientSecret: 'deliverburguer123'
 };
